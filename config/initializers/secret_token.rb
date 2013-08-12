e6476ab84f345ad7c33ed93776f5afb410058219
clemieux55 <clemieux598@gmail.com>
2013-08-12 19:25:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CsvFileRead::Application.config.secret_key_base = 'a6aa5febea0e40dccd67bb4efd4d9b31532cdde0369fb009c93053efa5cf7b9bc7411481227da76a3fc4ec70137dcbe998c01a425e283a37d4ff7868a4cf604b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CsvFileRead::Application.config.session_store :cookie_store, key: '_csv_file_read_session'
