da4589e56e52b0c576604b1dc621e90996631de8
clemieux55 <clemieux598@gmail.com>
2013-08-12 20:44:58 -0400

    person spec returns multi-dimentional array

--- spec/models/file_spec.rb
+++ spec/models/file_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
 describe Person do 
+	let(:person) { Person.read_csv_to_database("public/test.csv") }
 
 	it 'returns an array' do 
-		expect(Person.read_csv_to_database("public/test.csv")).to be_kind_of Array
+		expect(person).to be_kind_of Array
+	end
+
+	it "returns a multidimentional array" do 
+		expect(person.length).to eql 2
 	end
 
 end
