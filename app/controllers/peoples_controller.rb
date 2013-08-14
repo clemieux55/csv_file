07dbd808da374e5af3a805ac89b7336e20894d2e
clemieux55 <clemieux598@gmail.com>
2013-08-13 22:40:34 -0400

    sort option field created

--- app/controllers/peoples_controller.rb
+++ app/controllers/peoples_controller.rb
@@ -2,7 +2,15 @@ class PeoplesController < ApplicationController
 
 	def index
 		@people = Person.all
+		@persons = @people.order(:sort_option)
+	end
 
+	def gender
+		@gender = Person.order("gender")
+	end
+
+	def last_name
+		@last_name = Person.order("last_name")
 	end
 
 end
